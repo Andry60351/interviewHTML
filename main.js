830ca45ad9bc6fd6c37e80c001e55cec64f36c6d
andry60351 <andryethemi@gmail.com>
2020-08-17 17:55:53 -0700

    various improvements

--- main.js
+++
@@ -1,7 +0,0 @@
-function testFunction(username, password) {
-    if (username == "Testname" && password == "demoPassword") {
-        alert("Successful login")
-    } else {
-        alert("Unsuccesul login")
-    }
-}
